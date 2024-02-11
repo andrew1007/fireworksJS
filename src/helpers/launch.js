efcb0f6928acaa6edab3df49b507257d0b7a75f6
andrew.yueh <yueh.andrew@gmail.com>
2024-02-11 13:07:47 -0800

    misc updates

--- src/helpers/launch.js
+++ src/helpers/launch.js
@@ -8,8 +8,8 @@ const ParticleCircle = require("../particles/particle_circle");
 const ParticleChain = require("../particles/particle_chain");
 
 class Launch {
-  constructor(x, y, ctx, canvas){
-    this.rockets=[]
+  constructor(x, y, ctx, canvas) {
+    this.rockets = []
     this.particles = []
     this.context = ctx
     this.canvas = canvas
@@ -30,34 +30,34 @@ class Launch {
     return [x, y, this.context, this.canvas, this.color]
   }
 
-  rocketStreamer(xPos, yPos){
+  rocketStreamer(xPos, yPos) {
     let rocket = new RocketStreamer(...this.rockArgs(xPos, yPos))
     this.rockets = this.rockets.concat(rocket)
   }
 
-  rocketStreak(xPos, yPos){
+  rocketStreak(xPos, yPos) {
     let rocket = new RocketStreak(...this.rockArgs(xPos, yPos))
     this.rockets = this.rockets.concat(rocket)
   }
 
-  rocketChain(xPos, yPos){
+  rocketChain(xPos, yPos) {
     let rocket = new RocketChain(...this.rockArgs(xPos, yPos))
     this.rockets = this.rockets.concat(rocket)
   }
 
-  rocketDefault(xPos, yPos){
+  rocketDefault(xPos, yPos) {
     let rocket = new Rocket(...this.rockArgs(xPos, yPos))
     this.rockets = this.rockets.concat(rocket)
   }
 
-  addFirework(e){
+  addFirework(e) {
     let streamerChance = (rng) => rng > this.chanceRocketStreamer
     let streakChance = (rng) => rng > this.chanceRocketStreak
     let chainChance = (rng) => rng > this.chanceRocketChain
     let xPos = this.x;
-    let yPos= this.y;
+    let yPos = this.y;
     let rng = Math.random()
-    switch(true){
+    switch (true) {
       case streamerChance(rng):
         this.rocketStreamer(xPos, yPos)
         break
@@ -72,8 +72,8 @@ class Launch {
     }
   }
 
-  welcomeFireworks(counter){
-    switch(counter){
+  welcomeFireworks(counter) {
+    switch (counter) {
       case 0:
         this.rocketDefault(this.x, this.y)
         break
@@ -89,7 +89,7 @@ class Launch {
     this.update()
   }
 
-  getRandomColor(){
+  getRandomColor() {
     let randColor = () => 80 + Math.round(Math.random() * 150)
     let a = 1
     let color = 'rgba('
@@ -106,76 +106,78 @@ class Launch {
     this.particles.push(object)
   }
 
-  particleCircle(firework, newColor = false){
+  particleCircle(firework, newColor = false) {
     let rng = Math.random()
-    for (let i=0; i < this.particleCircleCount; i++){
+    for (let i = 0; i < this.particleCircleCount; i++) {
       if (newColor) this.color = this.getRandomColor()
       this.particleCount += 45
       this.pushParticle(new ParticleCircle(...this.partArgs(firework)))
     }
   }
 
-  particleChain(firework){
+  particleChain(firework) {
     let rng = Math.random()
-    for(let i=0; i < this.particleChainCount; i++){
+    for (let i = 0; i < this.particleChainCount; i++) {
       if (rng > 0.60) this.color = this.getRandomColor()
       this.pushParticle(new ParticleChain(...this.partArgs(firework)))
     }
   }
 
-  particleDefault(firework, particleCount, newColor = false){
-    for(let i=0; i < particleCount; i++){
+  particleDefault(firework, particleCount, newColor = false) {
+    for (let i = 0; i < particleCount; i++) {
       if (newColor) this.color = this.getRandomColor()
       this.pushParticle(new ParticleDefault(...this.partArgs(firework)))
     }
   }
 
-  triggerParticles(){
+  triggerParticles() {
     this.rockets.forEach((firework, i) => {
-      if (firework.constructor.name === "RocketStreamer"){
+      if (firework.constructor.name === "RocketStreamer") {
         if (firework.trigger()) this.particleDefault(firework, 20, true)
       }
       let rng = Math.random()
-        if (firework.exploded()){
-          switch(firework.constructor.name){
-            case "RocketStreak":
-              this.particleCircle(firework)
+      console.log(firework.exploded())
+      if (firework.exploded()) {
+        console.log('EXPLODED')
+        switch (firework.constructor.name) {
+          case "RocketStreak":
+            this.particleCircle(firework)
             break
-            case "RocketChain":
-              this.particleChain(firework)
+          case "RocketChain":
+            this.particleChain(firework)
             break
-            case "RocketStreamer":
-              this.particleCircle(firework, true)
+          case "RocketStreamer":
+            this.particleCircle(firework, true)
             break
-            case "Rocket":
-              this.particleDefault(firework, 30)
+          case "Rocket":
+            this.particleDefault(firework, 30)
             break
-          }
-          this.rockets.splice(i, 1)
         }
-        firework.render()
-        firework.update()
-      })
+        this.rockets.splice(i, 1)
+      }
+      firework.render()
+      firework.update()
+    })
   }
 
-  clearParticles(){
+  clearParticles() {
     this.particles.forEach((particle, i) => {
       if (!particle.exists()) {
         let rng = Math.random()
-        if (particle.constructor.name === "ParticleChain"){
+        if (particle.constructor.name === "ParticleChain") {
           if (rng > 0.80) this.color = this.getRandomColor()
           this.particleDefault(particle, 20)
         }
-      this.particles.splice(i, 1)
-      this.particleCount -= 1
+        this.particles.splice(i, 1)
+        this.particleCount -= 1
       }
       particle.render()
       particle.update()
     })
   }
 
-  update(){
-    if (this.particles.length >  1 || this.rockets.length > 0){
+  update() {
+    if (this.particles.length > 1 || this.rockets.length > 0) {
       requestAnimationFrame(() => this.update())
     }
     this.triggerParticles()
--- src/rockets/giantStatic.js
+++
@@ -1,7 +0,0 @@
-let RocketBaseClass = require('./rocket_base_class')
-
-class RocketStreak extends RocketBaseClass {
-    render() {
-        this.
-    }
-}
