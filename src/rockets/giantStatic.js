485d9df6821377718381c0f19e7b43efbba871fd
andrew.yueh <yueh.andrew@gmail.com>
2024-02-11 12:59:48 -0800

    rename

---
+++ src/rockets/giantStatic.js
@@ -0,0 +1,7 @@
+let RocketBaseClass = require('./rocket_base_class')
+
+class RocketStreak extends RocketBaseClass {
+    render() {
+        this.
+    }
+}
---
+++ src/webpack.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+/**
+ * @typedef {import('webpack').Configuration} Configuration
+ */
+
+/**
+ * @type {Configuration}
+ */
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    publicPath: './',
+    filename: 'fireworks.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
